3828a86d14da7e5eb42e181b625c4787fd800672
Corey P <cparsons0730@yahoo.com>
2025-06-25 12:04:10 -0500

    removing duplicate functions/typos

--- script.js
+++ script.js
@@ -37,32 +37,6 @@ console.log(getGrade(74)); // C
 console.log(getGrade(60)); // D
 console.log(getGrade(45)); // F
 
-function getAverage(scores) {
-  let sum = 0;
-
-  for (const score of scores) {
-    sum += score;
-  }
-
-  return sum / scores.length;
-}
-
-function getGrade(score) {
-  if (score === 100) {
-    return "A++";
-  } else if (score >= 90) {
-    return "A";
-  } else if (score >= 80) {
-    return "B";
-  } else if (score >= 70) {
-    return "C";
-  } else if (score >= 60) {
-    return "D";
-  } else {
-    return "F";
-  }
-}
-
 function hasPassingGrade(score) {
   if (getGrade(score) === "F") {
     return false;
@@ -72,36 +46,6 @@ function hasPassingGrade(score) {
 console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
-
-function getAverage(scores) {
-  let sum = 0;
-
-  for (const score of scores) {
-    sum += score;
-  }
-
-  return sum / scores.length;
-}
-
-function getGrade(score) {
-  if (score === 100) {
-    return "A++";
-  } else if (score >= 90) {
-    return "A";
-  } else if (score >= 80) {
-    return "B";
-  } else if (score >= 70) {
-    return "C";
-  } else if (score >= 60) {
-    return "D";
-  } else {
-    return "F";
-  }
-}
-
-function hasPassingGrade(score) {
-  return getGrade(score) !== "F";
-}
 
 function studentMsg(totalScores, studentScore) {
   const average = getAverage(totalScores);
